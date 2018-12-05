4b5549e68cb9fc1344a2e923959d87bb956214c6
Gil Mendes <gil00mendes@gmail.com>
2018-12-05 23:48:23 +0000

    Add day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const PUZZLE: &str = include_str!("input.txt");
 
@@ -6,6 +6,11 @@ fn main() {
     let claims = PUZZLE.lines().collect::<Vec<&str>>();
 
     let mut cuts: HashMap<(i32, i32), i32> = HashMap::new();
+
+    let mut overlap_ids: HashMap<(i32, i32), i32> = HashMap::new();
+
+    let mut overlap_cuts = HashSet::new();
+    let mut all_cuts = HashSet::new();
 
     claims.iter().for_each(|claim| {
         // Format: [0:ID, 1:fromTop, 2:fromLeft, 3:width, 4:height]
@@ -17,10 +22,24 @@ fn main() {
         for x in params[1]..params[1]+params[3] {
             for y in params[2]..params[2]+params[4] {
                 *cuts.entry((x, y)).or_insert(0) += 1;
+
+                // Insert all cuts on a hash set and register all overlapped claims into
+                // another set to perform a diff between the two at the end
+                all_cuts.insert(params[0]);
+
+                if overlap_ids.contains_key(&(x, y)) {
+                    overlap_cuts.insert(overlap_ids[&(x, y)]);
+                    overlap_cuts.insert(params[0]);
+                } else {
+                    overlap_ids.insert((x, y), params[0]);
+                }
             }
         }
     });
 
     let overlap = cuts.values().filter(|v| **v > 1).count();
     println!("Overlap: {}", overlap);
+
+    let non_overlapped: HashSet<_> = all_cuts.difference(&overlap_cuts).collect();
+    println!("Non Overlap: {:?}", non_overlapped);
 }
