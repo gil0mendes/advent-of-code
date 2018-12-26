91fa471d15725835bb40b1cf7ba60af7d53cc2ba
Gil Mendes <gil00mendes@gmail.com>
2018-12-26 00:39:38 +0000

    Add day 7 part 2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -53,11 +53,12 @@ impl FromStr for Dependency {
     }
 }
 
-fn find_next_step(requirements: &Required, done: &HashSet<Step>) -> Option<Step> {
+/// Find the possible next steps based on the current done and taken steps
+fn find_next_step(requirements: &Required, done: &HashSet<Step>, taken: &HashSet<Step>) -> Vec<Step> {
     let mut possible_steps: Vec<Step> = Vec::new();
 
     for (&step, deps) in requirements {
-        if done.contains(&step) {
+        if taken.contains(&step) {
             continue;
         }
 
@@ -69,7 +70,7 @@ fn find_next_step(requirements: &Required, done: &HashSet<Step>) -> Option<Step>
     possible_steps.sort();
     possible_steps.dedup();
     possible_steps.reverse();
-    possible_steps.pop()
+    possible_steps
 }
 
 fn part1(requirements: &Required) {
@@ -77,9 +78,9 @@ fn part1(requirements: &Required) {
     let mut done: HashSet<Step> = HashSet::new();
 
     loop {
-        let next_step = find_next_step(&requirements, &done);
+        let mut next_step = find_next_step(&requirements, &done, &done);
 
-        let next_step = match next_step {
+        let next_step = match next_step.pop() {
             None => break,
             Some(next_step) => next_step,
         };
@@ -90,6 +91,101 @@ fn part1(requirements: &Required) {
 
     let result: String = order.iter().collect();
     println!("Part1: {}", result);
+}
+
+/// Enum that represents the status of a worker
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Status {
+    /// That means the worker in on a Idle state.
+    Idle,
+    /// That contains the step and the remaining seconds
+    /// for the worker finish the job.
+    Working {
+        step: Step,
+        remaining: u32,
+    },
+}
+
+fn apply_tick(workers: &mut Vec<Status>) -> (Vec<Step>) {
+    let mut finished_steps: Vec<Step> = Vec::new();
+
+    for index in 0..workers.len() {
+        match workers[index] {
+            Status::Idle => {}
+            Status::Working { step, ref mut remaining } => {
+                *remaining -= 1;
+                if *remaining == 0 {
+                    finished_steps.push(step);
+                    workers[index] = Status::Idle;
+                }
+            }
+        }
+    }
+
+    finished_steps
+}
+
+/// Check if all workers are idle
+fn all_workers_idle(workers: &Vec<Status>) -> bool {
+    workers.iter().all(|&w| w == Status::Idle)
+}
+
+/// Get the index of all available workers
+fn available_workers(workers: &Vec<Status>) -> Vec<usize> {
+    let mut available: Vec<usize> = Vec::new();
+
+    for index in 0..workers.len() {
+        let worker = workers[index];
+        if worker == Status::Idle {
+            available.push(index);
+        }
+    }
+
+    available
+}
+
+fn compute_step_time(step: &Step) -> u32 {
+    (*step as u32) - b'A' as u32 + 1 + 60
+}
+
+fn part2(requirements: &Required) {
+    // Set that contains the assigned steps
+    let mut assigned: HashSet<Step> = HashSet::new();
+
+    // Contains all the registered workers
+    let mut workers: Vec<Status> = vec![Status::Idle; 5];
+
+    // Hash set with all finished tasks
+    let mut done: HashSet<Step> = HashSet::new();
+
+    let mut seconds: u32 = 0;
+    loop {
+        let done_steps = apply_tick(&mut workers);
+        done.extend(done_steps);
+        println!("done => {:?}", done);
+        let mut next_tasks = find_next_step(&requirements, &done, &assigned);
+
+        // Check if everything is done in order to break
+        if next_tasks.is_empty() && all_workers_idle(&workers) {
+            break;
+        }
+
+        for index in available_workers(&workers) {
+            let step = match next_tasks.pop() {
+                None => break,
+                Some(step) => step,
+            };
+            workers[index] = Status::Working {
+                step,
+                remaining: compute_step_time(&step),
+            };
+            assigned.insert(step);
+        }
+
+        seconds += 1;
+    }
+
+    println!("Part2: {}", seconds);
 }
 
 fn main() {
@@ -110,4 +206,5 @@ fn main() {
         });
 
     part1(&requirements);
+    part2(&requirements);
 }
