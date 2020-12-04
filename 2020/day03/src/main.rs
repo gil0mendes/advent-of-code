6bf6708530433734a890b312e7783bf3564263d5
Gil Mendes <gil00mendes@gmail.com>
2020-12-04 20:07:56 +0000

    solve day03 part 2

--- 2020/day03/src/main.rs
+++ 2020/day03/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(iterator_fold_self)]
+
 use std::fs;
 
 #[derive(Debug)]
@@ -13,19 +15,36 @@ fn chart_to_tile(ch: char) -> Tile {
     }
 }
 
-fn number_of_trees(map: &Vec<Vec<Tile>>) -> u32 {
-    let mut col = 0;
+fn get_number_of_trees(map: &Vec<Vec<Tile>>, (down, right): (u32, u32)) -> u32 {
+    let mut x = right;
+    let mut y = down;
+    let mut number_of_trees: u32 = 0;
 
-    map.iter()
-        .skip(1)
-        .map(|line| {
-            col = (col + 3) % line.len();
-            match line.get(col).unwrap() {
-                Tile::Open => 0,
-                Tile::Tree => 1,
-            }
-        })
-        .sum()
+    loop {
+        let line = map.get(y as usize).unwrap();
+
+        number_of_trees += match line.get(x as usize).unwrap() {
+            Tile::Open => 0,
+            Tile::Tree => 1,
+        };
+
+        x = (x + right) % line.len() as u32;
+        y += down;
+
+        if y as usize >= map.len() {
+            return number_of_trees;
+        }
+    }
+}
+
+fn number_of_trees_on_slope(map: &Vec<Vec<Tile>>) -> usize {
+    let slopes_to_test = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)];
+
+    slopes_to_test
+        .iter()
+        .map(|&slope| get_number_of_trees(map, slope) as usize)
+        .fold_first(|acc, r| acc * r)
+        .unwrap()
 }
 
 fn main() {
@@ -37,5 +56,6 @@ fn main() {
         .map(|line| line.chars().map(chart_to_tile).collect())
         .collect();
 
-    println!("Part 1: {}", number_of_trees(&map));
+    println!("Part 1: {}", get_number_of_trees(&map, (1, 3)));
+    println!("Part 2: {}", number_of_trees_on_slope(&map));
 }
