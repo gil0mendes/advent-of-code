ec359c85befc7c7bd319c6755b4e77efa67e3e43
Gil Mendes <gil00mendes@gmail.com>
2023-12-02 14:03:54 +0000

    Add 2013 day2 part2

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -67,8 +67,12 @@ fn compute_part1(raw_data: &String) -> u32 {
         .sum()
 }
 
-fn compute_part2(_raw_data: &String) -> u32 {
-    0
+fn compute_part2(raw_data: &String) -> u32 {
+    raw_data
+        .lines()
+        .map(|line| Game::from_str(line).unwrap())
+        .map(|game| game.red * game.green * game.blue)
+        .sum()
 }
 
 fn main() {
@@ -92,8 +96,12 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
 #[test]
 fn test_part2() -> Result<(), ()> {
-    const INPUT: &str = "";
+    const INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
-    assert_eq!(0, compute_part2(&INPUT.to_string()));
+    assert_eq!(2286, compute_part2(&INPUT.to_string()));
     Ok(())
 }
